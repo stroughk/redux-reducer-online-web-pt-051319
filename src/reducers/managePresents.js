7ac87f2717a897b1334a7c6c5f04893038ae954c
Kateryna Strough <katerynastrough@hotmail.com>
2020-02-26 21:09:12 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,12 +1,12 @@
 export function managePresents(state = {
   numberOfPresents: 0,
-}, action){
-  switch (action.type) {
+}, action) {
+  switch(action.type) {
+
     case "INCREASE":
       return Object.assign({}, state, {
         numberOfPresents: state.numberOfPresents + 1
       });
-
 
     default:
       return state;
